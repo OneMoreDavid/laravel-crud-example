11c530cdaf010cc887af214e962f3dbac11ca026
David Dickinson <rnddave@gmail.com>
2024-10-21 11:21:34 +0100

    the layout file

--- laravel-crud-example/routes/web.php
+++ laravel-crud-example/routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('posts/', function () {
+    return view('index');
 });
 
 Route::resource('posts', PostController::class);
